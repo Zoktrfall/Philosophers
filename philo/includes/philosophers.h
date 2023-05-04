a603edf24dcc033a2a2f5904c96ebbb0724893b9
ZoktrFall <shahavetgnum@gmail.com>
2023-05-04 20:49:13 +0400

    Start Philogit status

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,11 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <string.h>
+
+#endif
